6b8ba4b72edbb1d55daf5ece7542a6c19c95dcc4
weilerv <weilervirag@gmail.com>
2024-02-29 19:17:53 +0100

    corrected test_main

--- test_main.py
+++ test_main.py
@@ -10,6 +10,7 @@ client = TestClient(app)
 def test_get():
     request = client.get("/")
     assert request.status_code == 200, 'response failed'
+    assert request.json() == {"greeting": "Welcome to my salary prediction api"}
 
 def test_post_one():
     input_data = {'age': 50,
@@ -30,25 +31,25 @@ def test_post_one():
     response = client.post('/prediction/', json=input_data)
 
     assert response.status_code == 200, f'Status code {response.json()} returned instead of 200'
-    assert response.json() == {"prediction:": [">50K"]}, "wrong prediction: expected <=50K, but the result was >50K"
+    assert response.json() == {"prediction:": ["<=50K"]}, "wrong prediction: expected <=50K, but the result was >50K"
 
 def test_post_two():
-    input_data = {'age': 38,
-                  'workclass': 'Provate',
-                  'fnlgt': 215646,
+    input_data = {'age': 52,
+                  'workclass': 'Self-emp-not-inc',
+                  'fnlgt': 209642,
                   'education': 'HS-grad',
                   'education-num': 9,
-                  'marital-status': 'Divorced',
-                  'occupation': 'Handler-cleaners',
-                  'relationship': 'Not-in-family',
+                  'marital-status': 'Married-civ-spouse',
+                  'occupation': 'Exec-managerial',
+                  'relationship': 'Husband',
                   'race': ' White',
                   'sex': ' Male',
                   'capital-gain': 0,
                   'capital-loss': 0,
-                  'hours-per-week': 40,
+                  'hours-per-week': 45,
                   'native-country': 'United-States'
                   }
     response = client.post('/prediction/', json=input_data)
 
     assert response.status_code == 200, f'Status code {response.json()} returned instead of 200'
-    assert response.json() == {"prediction:": [">50K"]}, "wrong prediction: expected <=50K, but the result is >50K"
+    assert response.json() == {"prediction:": [">50K"]}, "wrong prediction: expected >50K, but the result is <=50K"
--- train_model.py
+++ train_model.py
@@ -7,8 +7,14 @@ import pickle, os
 # Add the necessary imports for the starter code.
 import pandas as pd
 import logging
-from .ml.model import train_model, compute_model_metrics, inference, perf_slices
-from .ml.data import process_data
+try:
+    from .ml.model import train_model, compute_model_metrics, inference, perf_slices
+except:
+    from ml.model import train_model, compute_model_metrics, inference, perf_slices
+try:
+    from .ml.data import process_data
+except:
+    from ml.data import process_data
 
 logging.basicConfig(filename='logging.log',
                     level=logging.INFO,
@@ -19,7 +25,6 @@ data = pd.read_csv('data/cleaned_census.csv')
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
-
 cat_features = [
     "workclass",
     "education",
@@ -41,18 +46,18 @@ X_test, y_test, _, _ = process_data(
 
 # check if trained model exists
 model_path = r'C:\Users\z0176083\OneDrive - ZF Friedrichshafen AG\Documents\Udacity\ML_DevOps\Section_4_deployment\nd0821-c3-starter-code-master\starter\model'
-#if model exists, load the model
+'''#if model exists, load the model
 if os.path.isfile(os.path.join(model_path,'trained_model.pkl')):
     model = pickle.load(open(os.path.join(model_path,'trained_model.pkl'), 'rb'))
     encoder = pickle.load(open(os.path.join(model_path,'encoder.pkl'), 'rb'))
     lb = pickle.load(open(os.path.join(model_path,'labelizer.pkl'), 'rb'))
 # Train and save a model.
-else:
-    model = train_model(X_train,y_train)
-    pickle.dump(model, open(os.path.join(model_path,'trained_model.pkl'),'wb'))
-    pickle.dump(encoder, open(os.path.join(model_path,'encoder.pkl'),'wb'))
-    pickle.dump(lb, open(os.path.join(model_path,'labelizer.pkl'),'wb'))
-    logging.info(f"Model saved to: {model_path}")
+else:'''
+model = train_model(X_train,y_train)
+pickle.dump(model, open(os.path.join(model_path,'trained_model.pkl'),'wb'))
+pickle.dump(encoder, open(os.path.join(model_path,'encoder.pkl'),'wb'))
+pickle.dump(lb, open(os.path.join(model_path,'labelizer.pkl'),'wb'))
+logging.info(f"Model saved to: {model_path}")
 
 
 #evaluate trained model on test set
@@ -62,7 +67,6 @@ logging.info(f"Classification target labels: {list(lb.classes_)}")
 logging.info(
     f"precision:{precision:.3f}, recall:{recall:.3f}, fbeta:{fbeta:.3f}")
 
-print(test)
 #compute performance on categorical slices. save results to slice_output.txt
 perf_slices(test, encoder, lb, model, cat_features, save_slice=True)
 logging.info(f"Performance on slices done")
