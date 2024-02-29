7b667a6230d009cb24c6ee7a886f94a82094e718
weilerv <weilervirag@gmail.com>
2024-02-29 11:25:00 +0100

    added screenshots

--- sample-request.py
+++ sample-request.py
@@ -5,16 +5,16 @@ data =  {  'age':37,
                 'workclass':"Private", 
                 'fnlgt':284582,
                 'education':"Masters",
-                'education_num':14,
-                'marital_status':"Married-civ-spouse",
+                'education-num':14,
+                'marital-status':"Married-civ-spouse",
                 'occupation':"Exec-managerial",
                 'relationship':"Wife",
                 'race':"White",
                 'sex':"Female",
-                'capital_gain':0,
-                'capital_loss':0,
-                'hours_per_week':40,
-                'native_country':"United-States"
+                'capital-gain':0,
+                'capital-loss':0,
+                'hours-per-week':40,
+                'native-country':"United-States"
         }
 
 r = requests.post("https://ml-devops-4.onrender.com/prediction", data=json.dumps(data))
